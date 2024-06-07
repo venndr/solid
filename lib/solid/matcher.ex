3e2888e632dd09630824b3c12a59ed53c8188530
Niklas Lindgren <niklas@musicglue.com>
2024-06-07 14:40:16 +0300

    stash

--- lib/solid/matcher.ex
+++ lib/solid/matcher.ex
@@ -4,74 +4,123 @@ defprotocol Solid.Matcher do
   def match(_, _)
 end
 
-defimpl Solid.Matcher, for: Any do
-  def match(data, []), do: {:ok, data}
-
-  def match(_, _), do: {:error, :not_found}
-end
-
-defimpl Solid.Matcher, for: List do
-  def match(data, []), do: {:ok, data}
-
-  def match(data, ["size"]) do
-    {:ok, Enum.count(data)}
-  end
-
-  def match(data, [key | keys]) when is_integer(key) do
-    case Enum.fetch(data, key) do
-      {:ok, value} -> @protocol.match(value, keys)
-      _ -> {:error, :not_found}
-    end
-  end
-end
-
-defimpl Solid.Matcher, for: Map do
-  def match(data, []) do
-    {:ok, data}
-  end
-
-  def match(data, ["size"]) do
-    {:ok, Map.get(data, "size", Enum.count(data))}
-  end
-
-  def match(data, [key | []]) do
-    case Map.fetch(data, key) do
-      {:ok, value} -> {:ok, value}
-      _ -> {:error, :not_found}
-    end
-  end
-
-  def match(data, [key | keys]) do
-    case Map.fetch(data, key) do
-      {:ok, value} -> @protocol.match(value, keys)
-      _ -> {:error, :not_found}
-    end
-  end
-end
-
-defimpl Solid.Matcher, for: BitString do
-  def match(current, []), do: {:ok, current}
-
-  def match(data, ["size"]) do
-    {:ok, String.length(data)}
-  end
-
-  def match(_data, [i | _]) when is_integer(i) do
-    {:error, :not_found}
-  end
-
-  def match(_data, [i | _]) when is_binary(i) do
-    {:error, :not_found}
-  end
-end
-
-defimpl Solid.Matcher, for: Atom do
-  def match(current, []) when is_nil(current), do: {:ok, nil}
-  def match(data, []), do: {:ok, data}
-  def match(nil, _), do: {:error, :not_found}
-
+defmodule Solid.Matcher.Builtins do
   @doc """
-  Matches all remaining cases
+  Solid comes with built-in matchers for the List, Map, BitString and Atom types, as well as the
+  fallback matcher for the Any type.
+
+  The using macro supports options to selectively include (`only`) and exclude (`omit`) individual
+  matchers.
+
+  The full list of available matchers is :any, :atom, :string, :list, :map
+
+  Examples:
+
+  # include all built-in matchers
+  use Solid.Matcher.Builtins
+
+  # selectively include only a subset
+  use Solid.Matcher.Builtins, only: [:any, :list]
+
+  # selectively exclude a subset
+  use Solid.Matcher.Builtins, omit: [:map, :atom]
   """
-  def match(_current, [key]) when is_binary(key), do: {:error, :not_found}
+
+  @all_matchers [:any, :atom, :bit_string, :list, :map]
+  @type matcher :: :any | :atom | :bit_string | :list | :map
+  @type option :: {:only, list(matcher())} | {:omit, list(matcher())}
+  @type options :: list(option())
+  @spec __using__(options()) :: Macro.t()
+  defmacro __using__(opts) do
+    excluded = Keyword.get(opts, :omit, [])
+
+    included =
+      opts
+      |> Keyword.get(:only, @all_matchers)
+      |> Enum.reject(fn m -> m in excluded end)
+
+    if :any in included do
+      defimpl Solid.Matcher, for: Any do
+        def match(data, []), do: {:ok, data}
+
+        def match(_, _), do: {:error, :not_found}
+      end
+    end
+
+    if :list in included do
+      defimpl Solid.Matcher, for: List do
+        def match(data, []), do: {:ok, data}
+
+        def match(data, ["size"]) do
+          {:ok, Enum.count(data)}
+        end
+
+        def match(data, [key | keys]) when is_integer(key) do
+          case Enum.fetch(data, key) do
+            {:ok, value} -> @protocol.match(value, keys)
+            _ -> {:error, :not_found}
+          end
+        end
+      end
+    end
+
+    if :map in included do
+      defimpl Solid.Matcher, for: Map do
+        def match(data, []) do
+          {:ok, data}
+        end
+
+        def match(data, ["size"]) do
+          {:ok, Map.get(data, "size", Enum.count(data))}
+        end
+
+        def match(data, [key | []]) do
+          case Map.fetch(data, key) do
+            {:ok, value} -> {:ok, value}
+            _ -> {:error, :not_found}
+          end
+        end
+
+        def match(data, [key | keys]) do
+          case Map.fetch(data, key) do
+            {:ok, value} -> @protocol.match(value, keys)
+            _ -> {:error, :not_found}
+          end
+        end
+      end
+    end
+
+    if :string in included do
+      defimpl Solid.Matcher, for: [BitString, String] do
+        def match(current, []), do: {:ok, current}
+
+        def match(data, ["size"]) do
+          {:ok, String.length(data)}
+        end
+
+        def match(_data, [i | _]) when is_integer(i) do
+          {:error, :not_found}
+        end
+
+        def match(_data, [i | _]) when is_binary(i) do
+          {:error, :not_found}
+        end
+      end
+    end
+
+    if :atom in included do
+      quote do
+        defimpl Solid.Matcher, for: Atom do
+          def match(current, []) when is_nil(current), do: {:ok, nil}
+          def match(data, []), do: {:ok, data}
+          def match(nil, _), do: {:error, :not_found}
+
+          @doc """
+          Matches all remaining cases
+          """
+          def match(_current, [key]) when is_binary(key), do: {:error, :not_found}
+        end
+      end
+    end
+  end
 end
--- test/matcher_test.exs
+++ test/matcher_test.exs
@@ -1,38 +1,81 @@
 defmodule MatcherTest do
   use ExUnit.Case, async: true
 
-  defmodule UserProfile do
-    defstruct [:full_name]
+  describe "custom matchers" do
+    defmodule UserProfile do
+      defstruct [:full_name]
 
-    defimpl Solid.Matcher do
-      def match(user_profile, ["full_name"]), do: {:ok, user_profile.full_name}
+      defimpl Solid.Matcher do
+        def match(user_profile, ["full_name"]), do: {:ok, user_profile.full_name}
+      end
+    end
+
+    defmodule User do
+      defstruct [:email]
+
+      def load_profile(%User{} = _user) do
+        # implementation omitted
+        %UserProfile{full_name: "John Doe"}
+      end
+
+      defimpl Solid.Matcher do
+        def match(user, ["email"]), do: {:ok, user.email}
+
+        def match(user, ["profile" | keys]),
+          do: user |> User.load_profile() |> @protocol.match(keys)
+      end
+    end
+
+    test "should render protocolized struct correctly" do
+      template = ~s({{ user.email }}: {{ user.profile.full_name }})
+
+      context = %{"user" => %User{email: "test@example.com"}}
+
+      assert "test@example.com: John Doe" ==
+               template |> Solid.parse!() |> Solid.render!(context) |> to_string()
     end
   end
 
-  defmodule User do
-    defstruct [:email]
+  describe "built-in matchers" do
+    use Solid.Matcher.Builtins
 
-    def load_profile(%User{} = _user) do
-      # implementation omitted
-      %UserProfile{full_name: "John Doe"}
+    test "for maps" do
+      template = ~s({{ beep.boop }})
+
+      context = %{"beep" => %{"boop" => "beep boop!"}}
+
+      assert "beep boop!" ==
+               template |> Solid.parse!() |> Solid.render!(context) |> to_string()
     end
 
-    defimpl Solid.Matcher do
-      def match(user, ["email"]), do: {:ok, user.email}
+    test "for strings" do
+      template =
+        ~s(How long is {{piece_of_string}}? {{piece_of_string | size}}.)
 
-      def match(user, ["profile" | keys]),
-        do: user |> User.load_profile() |> @protocol.match(keys)
+      context = %{"piece_of_string" => "a piece of string"}
+
+      assert "How long is a piece of string? 17." ==
+               template |> Solid.parse!() |> Solid.render!(context) |> to_string()
     end
-  end
 
-  test "should render protocolized struct correctly" do
-    template = ~s({{ user.email }}: {{ user.profile.full_name }})
+    test "for lists" do
+      template = ~s(This eagle is just {{items | size}} {{items[1]}}s in a trenchcoat.)
 
-    context = %{
-      "user" => %User{email: "test@example.com"}
-    }
+      context = %{
+        "items" => ~w(This parrot has ceased to be.)
+      }
 
-    assert "test@example.com: John Doe" ==
-             template |> Solid.parse!() |> Solid.render!(context) |> to_string()
+      assert "This eagle is just 6 parrots in a trenchcoat." ==
+               template |> Solid.parse!() |> Solid.render!(context) |> to_string()
+    end
+
+    test "for atom" do
+      template = ~s({{molecule.atom.particle}})
+
+      context = %{"molecule" => %{"atom" => %{"particle" => :neutron}}}
+
+      assert "neutron" ==
+               template |> Solid.parse!() |> Solid.render!(context) |> to_string()
+    end
   end
 end
