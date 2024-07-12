a6c27354e71504467d29b422ba9e9a6e3b3d6627
Niklas Lindgren <niklas@musicglue.com>
2024-07-12 12:15:48 +0300

    feat(matchers): exclude bundled matchers by default
    commit 3188526e2fbc918446def040755ef3f4aa3a67c4
    Author: Niklas Lindgren <niklas@musicglue.com>
    Date:   Tue Jun 11 15:24:24 2024 +0300
    
        docs(readme): adds new section on including Solid in your project
    
        This documents the altered approach to including Solid a project, the
        changes to importing the default matchers and how to bring your own
        matchers for types with bundled matchers.
    
    commit ddcb0fdd367b6a3e0a7bedb96b856511e7efc74d
    Author: Niklas Lindgren <niklas@musicglue.com>
    Date:   Tue Jun 11 15:24:05 2024 +0300
    
        test(test helpers): add `use Solid` to test helpers
    
    commit 67c60c75a3c6fcb89b946d72a83cbb6664e4fa36
    Author: Niklas Lindgren <niklas@musicglue.com>
    Date:   Tue Jun 11 15:21:59 2024 +0300
    
        feat: export a `use Solid` macro for default config usage
    
        This is a convenience method, that makes including Solid in your project
        using the default config easier.
    
        This will be the simplest and likeliest upgrade path for most Solid
        users.
    
    commit 2dcd98024a8e5801d3546fe23d1816badbc76984
    Author: Niklas Lindgren <niklas@musicglue.com>
    Date:   Tue Jun 11 15:20:28 2024 +0300
    
        test(matchers): add cases for built-in matchers
    
    commit ac54b6b818a6166559a67940477d01c2cabff3e3
    Author: Niklas Lindgren <niklas@musicglue.com>
    Date:   Tue Jun 11 14:57:33 2024 +0300
    
        feat!(matchers): exclude bundled matchers by default
    
        Packages the bundled matchers in a new module `Solid.Matcher.Builtins`,
        adding a `__using__` macro for including them in your own project.
    
        This is a breaking change, but the only way for supporting custom
        implementations in place of the bundled matchers.
    
    commit 0a86872ee2b512ed8dbdda201bf57c1e6c701542
    Author: Matt Sutkowski <msutkowski@gmail.com>
    Date:   Mon Apr 29 21:14:23 2024 -0700
    
        chore: update docs around render/3 and README

--- lib/solid/matcher.ex
+++ lib/solid/matcher.ex
@@ -4,80 +4,130 @@ defprotocol Solid.Matcher do
   def match(_, _)
 end
 
-defimpl Solid.Matcher, for: Any do
-  def match(data, []), do: {:ok, data}
-
-  def match(_, _), do: {:error, :not_found}
-end
-
-defimpl Solid.Matcher, for: List do
-  def match(data, []), do: {:ok, data}
-
-  def match(data, ["first"]), do: {:ok, Enum.at(data, 0)}
-  def match(data, ["last"]), do: {:ok, Enum.at(data, -1)}
-  def match(data, ["size"]), do: {:ok, Enum.count(data)}
-
-  def match(data, [key | keys]) when is_integer(key) do
-    case Enum.fetch(data, key) do
-      {:ok, value} -> @protocol.match(value, keys)
-      _ -> {:error, :not_found}
-    end
-  end
-
-  def match(_data, _) do
-    {:error, :not_found}
-  end
-end
-
-defimpl Solid.Matcher, for: Map do
-  def match(data, []) do
-    {:ok, data}
-  end
-
-  # Maps are not ordered so these are here just for consistency with the Liquid implementation
-  # as we must return something
-
-  def match(data, [key | keys]) do
-    case Map.fetch(data, key) do
-      {:ok, value} ->
-        @protocol.match(value, keys)
-
-      _ ->
-        # Check if the key is a special case
-        case key do
-          "first" -> @protocol.match(Enum.at(data, 0), keys)
-          "size" -> @protocol.match(map_size(data), keys)
-          _ -> {:error, :not_found}
-        end
-    end
-  end
-end
-
-defimpl Solid.Matcher, for: BitString do
-  def match(current, []), do: {:ok, current}
-
-  def match(data, ["size"]) do
-    {:ok, String.length(data)}
-  end
-
-  def match(_data, [i | _]) when is_integer(i) do
-    {:error, :not_found}
-  end
-
-  def match(_data, [i | _]) when is_binary(i) do
-    {:error, :not_found}
-  end
-end
-
-defimpl Solid.Matcher, for: Atom do
-  def match(current, []) when is_nil(current), do: {:ok, nil}
-  def match(data, []), do: {:ok, data}
-  def match(nil, _), do: {:error, :not_found}
-
+defmodule Solid.Matcher.Builtins do
   @doc """
-  Matches all remaining cases
+  Solid comes with built-in matchers for the Atom, Map, List, and String/BitString types, as well
+  as a fallback matcher for the Any type.
+
+  The using macro supports options to selectively include (`:only`) and exclude (`:except`)
+  individual matchers, should you wish to replace all or a subset with a custom matcher.
+
+  The full list of available matchers is `:any`, `:atom`, `:list`, `:map`, `:string`
+
+  Examples:
+
+  # include all built-in matchers
+  use Solid.Matcher.Builtins
+
+  # selectively include only a subset
+  use Solid.Matcher.Builtins, only: [:any, :list]
+
+  # selectively exclude a subset
+  use Solid.Matcher.Builtins, except: [:map, :atom]
   """
-  def match(_current, [key]) when is_binary(key), do: {:error, :not_found}
+
+  @all_matchers [:any, :atom, :string, :list, :map]
+
+  @type matcher :: :any | :atom | :list | :map | :string
+  @type option :: {:only, list(matcher())} | {:except, list(matcher())}
+  @type options :: list(option())
+  @spec __using__(options()) :: Macro.t()
+  defmacro __using__(opts) do
+    excluded = Keyword.get(opts, :except, [])
+
+    included =
+      opts
+      |> Keyword.get(:only, @all_matchers)
+      |> Enum.reject(fn m -> m in excluded end)
+
+    quote do
+      if :list in unquote(included) do
+        defimpl Solid.Matcher, for: List do
+          def match(data, []), do: {:ok, data}
+
+          def match(data, ["first"]), do: {:ok, Enum.at(data, 0)}
+          def match(data, ["last"]), do: {:ok, Enum.at(data, -1)}
+          def match(data, ["size"]), do: {:ok, Enum.count(data)}
+
+          def match(data, [key | keys]) when is_integer(key) do
+            case Enum.fetch(data, key) do
+              {:ok, value} -> @protocol.match(value, keys)
+              _ -> {:error, :not_found}
+            end
+          end
+
+          def match(_data, _) do
+            {:error, :not_found}
+          end
+        end
+      end
+
+      if :map in unquote(included) do
+        defimpl Solid.Matcher, for: Map do
+          def match(data, []) do
+            {:ok, data}
+          end
+
+          # Maps are not ordered so these are here just for consistency with the Liquid implementation
+          # as we must return something
+
+          def match(data, [key | keys]) do
+            case Map.fetch(data, key) do
+              {:ok, value} ->
+                @protocol.match(value, keys)
+
+              _ ->
+                # Check if the key is a special case
+                case key do
+                  "first" -> @protocol.match(Enum.at(data, 0), keys)
+                  "size" -> @protocol.match(map_size(data), keys)
+                  _ -> {:error, :not_found}
+                end
+            end
+          end
+        end
+      end
+
+      if :string in unquote(included) do
+        defimpl Solid.Matcher, for: [BitString, String] do
+          def match(current, []), do: {:ok, current}
+
+          def match(data, ["size"]) do
+            {:ok, String.length(data)}
+          end
+
+          def match(_data, [i | _]) when is_integer(i) do
+            {:error, :not_found}
+          end
+
+          def match(_data, [i | _]) when is_binary(i) do
+            {:error, :not_found}
+          end
+        end
+      end
+
+      if :atom in unquote(included) do
+        defimpl Solid.Matcher, for: Atom do
+          def match(current, []) when is_nil(current), do: {:ok, nil}
+          def match(data, []), do: {:ok, data}
+          def match(nil, _), do: {:error, :not_found}
+
+          @doc """
+          Matches all remaining cases
+          """
+          def match(_current, [key]) when is_binary(key), do: {:error, :not_found}
+        end
+      end
+
+      if :any in unquote(included) do
+        defimpl Solid.Matcher, for: Any do
+          def match(data, []), do: {:ok, data}
+
+          def match(_, _), do: {:error, :not_found}
+        end
+      end
+    end
+  end
 end
 
 defimpl Solid.Matcher, for: Tuple do
